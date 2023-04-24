67df4236a1c6d2689631dcc454c20867bf7ff463
ReeceRenninger <renningerreece@gmail.com>
2023-04-24 13:08:44 -0700

    got border to delete as timer counts down

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let durationInput = document.getElementById('duration');
 let startButton = document.getElementById('start');
 let pauseButton = document.getElementById('pause');
+let circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('timer has started!');
   },
   onTick() {
-    console.log('timer is counting down!')
+    circle.setAttribute('stroke-dashoffset', currentOffset )
+    currentOffset = currentOffset - 50;
   },
   onComplete() {
     console.log('timer is done');
